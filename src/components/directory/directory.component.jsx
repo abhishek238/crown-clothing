c65fff395d4082ca63c9dd6ef6bf71a774350c6a
Abhishek Agarwal <abhishekagarwal238@gmail.com>
2021-02-10 18:54:46 +0530

    basic routing set-up

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,61 @@
 import React from 'react';
+import MenuItem from "../menu-item/menu-item.component";
+import './directory.styles.scss';
 
-const MenuItem = ({title}) => (
-    <div className="menu-item">
-        <div className="content">
-            <div className="title">{title}</div>
-            <span className="subtitle">Shop Now</span>
-        </div>
-    </div>
-);
+class Directory extends React.Component {
 
-export  default MenuItem;
+    constructor() {
+        super();
+
+        this.state = {
+                sections: [
+                    {
+                        title: 'hats',
+                        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                        id: 1,
+                        linkUrl: 'shop/hats'
+                    },
+                    {
+                        title: 'jackets',
+                        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                        id: 2,
+                        linkUrl: 'shop/jackets'
+                    },
+                    {
+                        title: 'sneakers',
+                        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                        id: 3,
+                        linkUrl: 'shop/sneakers'
+                    },
+                    {
+                        title: 'womens',
+                        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                        size: 'large',
+                        id: 4,
+                        linkUrl: 'shop/womens'
+                    },
+                    {
+                        title: 'mens',
+                        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                        size: 'large',
+                        id: 5,
+                        linkUrl: 'shop/mens'
+                    }
+                ]
+        };
+    }
+
+    render() {
+        return (
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({id, ...otherProps}) => (
+                        <MenuItem key={id} {...otherProps}/>
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default Directory;
